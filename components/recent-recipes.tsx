00c2798054fd6c236169b9302fd5688fa096e628
Smart Ones <110102396+bsmartones@users.noreply.github.com>
2024-12-29 00:46:00 +0100

    Update recent-recipes.tsx

--- components/recent-recipes.tsx
+++ components/recent-recipes.tsx
@@ -5,7 +5,7 @@ export async function RecentRecipes() {
   const [recipes] = await Promise.all([getLatestRecipes()])
   return (
     <div className="duration-1200 ease-in-out animate-in fade-in slide-in-from-bottom-4">
-      <h2 className="text-2xl font-semibold tracking-tight">Recent Recipes</h2>
+      <h2 className="text-2xl font-semibold tracking-tight">Recent Strategies</h2>
       <div className="grid gap-2 md:grid-cols-3 md:gap-6">
         {recipes?.map((recipe) => (
           <div key={recipe.id}>
