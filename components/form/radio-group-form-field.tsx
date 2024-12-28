fdaecbfce04e2696d85af7f8ee61e84c9d9018a4
Smart Ones <110102396+bsmartones@users.noreply.github.com>
2024-12-29 00:43:26 +0100

    Update radio-group-form-field.tsx

--- components/form/radio-group-form-field.tsx
+++ components/form/radio-group-form-field.tsx
@@ -22,9 +22,9 @@ interface RadioGroupFormFieldProps {
 }
 
 export const options: RadioGroupOption[] = [
-  { label: "2 People", value: "2" },
-  { label: "4 People", value: "4" },
-  { label: "6 People", value: "6" },
+  { label: "Software (SAAS)", value: "2" },
+  { label: "Ecommerce Store", value: "4" },
+  { label: "Local Business", value: "6" },
 ]
 
 export function RadioGroupFormField({
